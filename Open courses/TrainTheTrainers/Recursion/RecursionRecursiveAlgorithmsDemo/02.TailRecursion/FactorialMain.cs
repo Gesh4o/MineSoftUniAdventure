dce68d363eef7e4cf49dbcfb544ea5a3142e1435
Gesh4o <georgi.stoimenov_96@abv.bg>
2016-06-03 15:05:22 +0300

    Recursion presentation update.

---
+++ Open courses/TrainTheTrainers/Recursion/RecursionRecursiveAlgorithmsDemo/02.TailRecursion/FactorialMain.cs
@@ -0,0 +1,39 @@
+﻿namespace _02.TailRecursion
+{
+    using System;
+
+    public class FactorialMain
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            // For n print the result of: result = n * (n-1) * (n - 2) * (n - 3) ... 1
+            long result;
+            result = GetResultRecursive(n);
+
+            // result = GetResultTailRecursive(n, 1);
+            Console.WriteLine(result);
+        }
+
+        private static long GetResultRecursive(int n)
+        {
+            if (n <= 1)
+            {
+                return 1;
+            }
+
+            return n * GetResultRecursive(n - 1);
+        }
+
+        private static long GetResultTailRecursive(int n, long result)
+        {
+            if (n <= 1)
+            {
+                return result;
+            }
+
+            return GetResultTailRecursive(n - 1, result * n);
+        }
+    }
+}
---
+++ Open courses/TrainTheTrainers/Recursion/RecursionRecursiveAlgorithmsDemo/04.DrawBinaryTree/DrawTreeForm.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace howto_binary_tree
+{
+    public partial class DrawTreeForm : Form
+    {
+        public DrawTreeForm()
+        {
+            InitializeComponent();
+        }
+
+        // Draw the tree.
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        // Recursively draw a binary tree branch.
+        private void DrawBranch(Graphics gr, Pen pen, int depth, float x, float y,
+            float length, float theta, float length_scale, float dtheta)
+        {
+            // See where this branch should end.
+            float x1 = (float)(x + length * Math.Cos(theta));
+            float y1 = (float)(y + length * Math.Sin(theta));
+
+            gr.DrawLine(pen, x, y, x1, y1);
+
+            // If depth > 1, draw the attached branches.
+            if (depth > 1)
+            {
+                DrawBranch(gr, pen, depth - 1, x1, y1,
+                    length * length_scale, theta + dtheta, length_scale,
+                    dtheta);
+                DrawBranch(gr, pen, depth - 1, x1, y1,
+                    length * length_scale, theta - dtheta, length_scale,
+                    dtheta);
+            }
+        }
+
+        // Draw the tree.
+        private void picCanvas_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(picCanvas.BackColor);
+            e.Graphics.SmoothingMode=System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+
+            try
+            {
+                float root_x = picCanvas.ClientSize.Width / 2;
+                float root_y = picCanvas.ClientSize.Height - 4;
+                float length_scale = float.Parse(txtLengthScale.Text);
+                float dtheta = (float)(Math.PI / 180.0 * (double)nudDtheta.Value);
+                DrawBranch(e.Graphics, Pens.Green,
+                    (int)nudDepth.Value, root_x, root_y,
+                    (int)nudLength.Value, (float)(-Math.PI / 2), length_scale,
+                    dtheta);
+            }
+            catch
+            {
+            }
+        }
+
+        // Redraw.
+        private void parameter_ValueChanged(object sender, EventArgs e)
+        {
+            picCanvas.Refresh();
+        }
+
+        // Redraw.
+        private void picCanvas_Resize(object sender, EventArgs e)
+        {
+            picCanvas.Refresh();
+        }
+
+        // Redraw.
+        private void nud_KeyUp(object sender, KeyEventArgs e)
+        {
+            picCanvas.Refresh();
+        }
+    }
+}
