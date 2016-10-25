b3a4a98d82ca7a8c3d3a5d6541150b875e855b68
Gesh4o <georgi.stoimenov_96@abv.bg>
2016-10-25 07:40:13 +0300

    [JS] CSS content connect.

--- Open courses/Express.js/02.ExpressJsViewEngines/index.js
+++ Open courses/Express.js/02.ExpressJsViewEngines/index.js
@@ -4,14 +4,27 @@ const routers = require('./scripts/routers/routers')
 const port = 1234
 const path = require('path')
 const bodyParser = require('body-parser')
+const stylus = require('stylus')
 
 app.set('view engine', 'pug')
 app.set('views', path.join(__dirname, 'views'))
 app.use(bodyParser.urlencoded({'extended': true}))
 
+app.use(express.static(path.join(__dirname, 'public')))
+
 app.use('/', routers.homeRouter)
 
 app.use('/posts', routers.postRouter)
+
+app.use(stylus.middleware({
+  src: '/public',
+  dest: path.join(__dirname, '/public/css/'),
+  compile: (str, path) => {
+    console.log(stylus(str))
+    return stylus(str).set('style', path)
+  }}))
+
+
 
 // app.post('posts/update/:id', (req, res) => {
 // })
